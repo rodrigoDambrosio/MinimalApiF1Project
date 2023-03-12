37ff7ceae0978cd387e28c30a9a81d76d00b75b1
Rodrigo DAmbrosio <rodrigo.dambrosio@gmail.com>
2023-03-11 22:38:43 -0300

    Fixed typo

--- DataAccess/DbAccess/ISqlDataAccess.cs
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -4,5 +4,5 @@ public interface ISqlDataAccess
     // This is useful because this doesn't care about what is behind, and how its being done, it can be mssql mongodb or whatever
     // The other project doesn't know how it works the data access
     Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "Default");
-    Task SaveData<T>(string storeProcedure, T paramaters, string connectionId = "Default");
+    Task SaveData<T>(string storeProcedure, T parameters, string connectionId = "Default");
 }
